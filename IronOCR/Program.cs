cbdf253152bbd5bcefb847171486d2e8638392d3
eshohag <eshohag@outlook.com>
2020-12-25 17:27:40 +0600

    OCR Final Code

--- IronOCR/Program.cs
+++ IronOCR/Program.cs
@@ -1,5 +1,8 @@
 ﻿using IronOcr;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace IronOCR
 {
@@ -7,14 +10,34 @@ namespace IronOCR
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Loading files...");
             var path = @"C:\Frelancing\ConsoleApp\OCR_Apps\IronOCR\Files\";
-            var ocr = new IronTesseract();
-            using (var input = new OcrInput(path + "Resume.jpg"))
+            List<string> paths = Directory.EnumerateFiles(path, "*.pdf").ToList();
+
+            var searchableKeyCv = new List<string>();
+
+            foreach (string filePath in paths)
             {
-                var result = ocr.Read(input);
-                Console.WriteLine(result.Text);
-                Console.ReadKey();
+                Console.WriteLine("Read File- " + filePath);
+                Console.WriteLine("Start-" + DateTime.Now);
+                var ocr = new IronTesseract();
+                using (var input = new OcrInput(filePath))
+                {
+                    var result = ocr.Read(input);
+                    var text = result.Text;
+                    if (text.Contains("React"))
+                        searchableKeyCv.Add("React");
+                    //if (text.Contains("ETL"))
+                    //    searchableKeyCv.Add("ETL");
+                    Console.WriteLine("End-" + DateTime.Now);
+                }
             }
+            Console.WriteLine("Searchable Key Result-");
+            foreach (var item in searchableKeyCv)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
         }
     }
 }
