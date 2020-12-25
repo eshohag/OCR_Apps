c051d2307d39e3bbbcd1cfbb0063e60848ba3c87
eshohag <eshohag@outlook.com>
2020-12-25 16:22:39 +0600

    Add project files.

---
+++ IronOCR/Program.cs
@@ -0,0 +1,20 @@
+﻿using IronOcr;
+using System;
+
+namespace IronOCR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Frelancing\ConsoleApp\OCR_Apps\IronOCR\Files\";
+            var ocr = new IronTesseract();
+            using (var input = new OcrInput(path + "Resume.jpg"))
+            {
+                var result = ocr.Read(input);
+                Console.WriteLine(result.Text);
+                Console.ReadKey();
+            }
+        }
+    }
+}
